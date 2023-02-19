4ffb1736b8ce79cc1f19954095a7b5e2168ab25c
Imesh Senanayake <imeshsenanayake1996@gmail.com>
2023-02-19 15:42:07 +0530

    2023/02/19

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Dash.css";
@@ -11,10 +10,9 @@ export default function () {
   const [hometown, setHometown] = useState("");
   const [error, setError] = useState(false);
 
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
 
   //const patient = { name, nic, mobile, hometown };
-  const facedt = {name};
 
   const handleclick = (e) => {
     e.preventDefault();
@@ -40,14 +38,14 @@ export default function () {
     }
   };
 
-  const face = () => {
+  const facedt = { nic };
+  const face = (e) => {
+    e.preventDefault();
     fetch("http://127.0.0.1:5000/add", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(facedt),
-      }).then(() => {
-        alert("Success");
-      });
+      method: "POST",
+      headers: { "Content-Type": "text/plain" },
+      body: facedt,
+    });
   };
   return (
     <div className="bar">
@@ -64,62 +62,66 @@ export default function () {
           <h1>Welcome</h1>
           <h3>Fill up your personal information</h3>
           <div className="form">
-            <input
-              type="text"
-              placeholder="Name with initials"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            {error && name.length <= 0 ? (
-              <label> Email can't be empty</label>
-            ) : (
-              ""
-            )}
-            <br />
-            <br />
-            <input
-              type="text"
-              placeholder="Nic number"
-              value={nic}
-              onChange={(e) => setNic(e.target.value)}
-            ></input>
-            {error && nic.length <= 0 ? (
-              <label> NIC can't be empty</label>
-            ) : (
-              ""
-            )}
-            <br />
-            <br />
-            <input
-              type="text"
-              placeholder="Mobile number"
-              value={mobile}
-              onChange={(e) => setMobile(e.target.value)}
-            ></input>
-            {error && mobile.length <= 0 ? (
-              <label> Mobile number can't be empty</label>
-            ) : (
-              ""
-            )}
-            <br />
-            <br />
-            <input
-              type="text"
-              placeholder="Hometown"
-              value={hometown}
-              onChange={(e) => setHometown(e.target.value)}
-            ></input>
-            {error && hometown.length <= 0 ? (
-              <label> Hometown can't be empty</label>
-            ) : (
-              ""
-            )}
-            <br />
-            <br />
-            <button onClick={face}>take face details</button>
-            <button className="btnsubmit" onClick={handleclick}>
-              Submit
-            </button>
+            <form action="http://127.0.0.1:5000/add" method="POST">
+              <input
+                type="text"
+                placeholder="Name with initials"
+                value={name}
+                name="name"
+                onChange={(e) => setName(e.target.value)}
+              ></input>
+              {error && name.length <= 0 ? (
+                <label> Email can't be empty</label>
+              ) : (
+                ""
+              )}
+              <br />
+              <br />
+              <input
+                type="text"
+                placeholder="Nic number"
+                value={nic}
+                name="nic"
+                onChange={(e) => setNic(e.target.value)}
+              ></input>
+              {error && nic.length <= 0 ? (
+                <label> NIC can't be empty</label>
+              ) : (
+                ""
+              )}
+              <br />
+              <br />
+              <input
+                type="text"
+                placeholder="Mobile number"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+              ></input>
+              {error && mobile.length <= 0 ? (
+                <label> Mobile number can't be empty</label>
+              ) : (
+                ""
+              )}
+              <br />
+              <br />
+              <input
+                type="text"
+                placeholder="Hometown"
+                value={hometown}
+                onChange={(e) => setHometown(e.target.value)}
+              ></input>
+              {error && hometown.length <= 0 ? (
+                <label> Hometown can't be empty</label>
+              ) : (
+                ""
+              )}
+              <br />
+              <br />
+              <button onClick={face}>take face details</button>
+              <button className="btnsubmit" >
+                Submit
+              </button>
+            </form>
           </div>
         </div>
       </div>
--- src/components/User.js
+++ src/components/User.js
@@ -3,6 +3,10 @@ import "./User.css"
 export default function User() {
     const [type,setType] = useState("");
     const [name,setName] = useState("");
+
+    const listnames=(e)=>{
+        console.log(type);
+    }
   return (
     <div className='user'>
         <h3>Welcome User</h3>
@@ -11,16 +15,17 @@ export default function User() {
         <h3>Select disease type</h3>
         <div className='type'>
         <select value={type}>
-                <option value='common diseases'>Common Diseases</option>
-                <option value='gastroenterological diseases'>Gastroenterological diseases</option>
-                <option value='respiratory diseases'>Respiratory diseases</option>
-                <option value='cardiovascular diseases'>Cardiovascular diseases</option>
-                <option value='urologic diseases'>Urologic diseases</option>
-                <option value='gynecological diseases'>Gynecological diseases</option>
-                <option value='andrological diseases'>Andrological diseases</option>
-                <option value='anorectal diseases'>Anorectal diseases</option>
-                <option value='vascular diseases'>Vascular diseases</option>
+                <option value='common diseases' onChange={(e)=>{setType(e.target.value)}}>Common Diseases</option>
+                <option value='gastroenterological diseases' onChange={(e)=>{setType(e.target.value)}}>Gastroenterological diseases</option>
+                <option value='respiratory diseases' onChange={(e)=>{setType(e.target.value)}}>Respiratory diseases</option>
+                <option value='cardiovascular diseases' onChange={(e)=>{setType(e.target.value)}}>Cardiovascular diseases</option>
+                <option value='urologic diseases' onChange={(e)=>{setType(e.target.value)}}>Urologic diseases</option>
+                <option value='gynecological diseases' onChange={(e)=>{setType(e.target.value)}}>Gynecological diseases</option>
+                <option value='andrological diseases' onChange={(e)=>{setType(e.target.value)}}>Andrological diseases</option>
+                <option value='anorectal diseases' onChange={(e)=>{setType(e.target.value)}}>Anorectal diseases</option>
+                <option value='vascular diseases' onChange={(e)=>{setType(e.target.value)}}>Vascular diseases</option>
             </select>
+            <button onClick={listnames}> Search</button>
         </div>
             
             <div className='name'>
